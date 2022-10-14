54fcd42e3824c0d967b791e4ae29ea888e8912c2
권지환 <zeno1030@naver.com>
2022-10-14 21:00:11 +0900

    deque

---
+++ 10866.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+size = int(input())
+a_list =deque()
+
+for i in range(size) : 
+    order = sys.stdin.readline().split()
+
+    if order[0]=="push_back":
+        a_list.append(order[1])
+        print(a_list)
+    if order[0]=="push_front":
+        a_list.appendleft(order[1])
+        print(a_list)
+    if order[0]=="pop_front":
+        if len(a_list) !=0 : print(a_list.popleft())
+        else : print(-1)
+    if order[0]=="pop_back":
+        if len(a_list) !=0 : print(a_list.pop())
+        else : print(-1)
+    if order[0]=="size":
+       print(len(a_list))
+    if order[0]=="empty":
+       if len(a_list) !=0 : print(0)
+       else : print(1)
+    if order[0] == "front":
+        if len(a_list) !=0 : print(a_list[0])
+        else : print(-1)
+    if order[0]=="back":
+        if len(a_list) !=0 : print(a_list[len(a_list)-1])
+        else : print(-1)
--- 3078.py
+++ 3078.py
@@ -9,10 +9,10 @@ for rank in range(n):
     name = len(stdin.readline().rstrip()) #학생들 이름 입력
     #print(name)
     students[rank] = name # 학생 등수와 이름의 길이 저장
-    print(students)
+    #print(students)
     if rank > k: # 학생의 등수가 K 보다 커지는 경우
         data[students[rank - k - 1]] -= 1 #자신과 상관 없는 등수의 학생을 제거
-        #print(data)
+    #print(data)
     count += data[name]     # 자신과 이름의 길이가 같은 친구를 쌍으로 추가
     #print(count)
     data[name] += 1 # 이름의 길이를 저장하는 리스트에 자신을 추가
